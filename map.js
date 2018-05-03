84fe68aa3fbb71c8fff1959ac876aea6a2fbf641
mariusholen <35805806+mariusholen@users.noreply.github.com>
2018-05-03 18:39:08 +0200

    Update map.js

--- map.js
+++ map.js
@@ -7,29 +7,9 @@ function initMap() {
       lat: 58.147,lng: 7.995}
     });
     
-  // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-        infoWindow.open(map);
-  
     map.data.loadGeoJson('data.json');
+  
+    map.data.loadGeoJson('data2.json');
 
   const infoWindow = new google.maps.InfoWindow();
   
