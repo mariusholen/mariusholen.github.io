12b396c5a34f1b550ceb19065e5267d4f6331385
mariusholen <35805806+mariusholen@users.noreply.github.com>
2018-05-03 14:24:58 +0200

    Update map.js

--- map.js
+++ map.js
@@ -7,6 +7,28 @@ function initMap() {
       lat: 58.147,lng: 7.995}
     });
     
+  // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+        infoWindow.open(map);
+  
     map.data.loadGeoJson('data.json');
 
   const infoWindow = new google.maps.InfoWindow();
@@ -24,3 +46,10 @@ function initMap() {
   });
   
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+  }
