fde2ed055c851610d8991d62fd3d42cd31006c3d
mariusholen <35805806+mariusholen@users.noreply.github.com>
2018-05-06 18:47:08 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,21 +1,32 @@
-	var map;
+var map;
       var infoWindow;
       var infowindow;
 
-      function initMap() { 
+     function initMap() {
       
-       // Try HTML5 geolocation.
+      // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
+       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
+      
 
-      infoWindow.setPosition(pos);
+     infoWindow.setPosition(pos);
       infoWindow.setContent('Location found.');
       infoWindow.open(map);
       map.setCenter(pos);
+      
+      infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: pos,
+          radius: 500,
+          keyword: ['matbutikk']
+        }, callback);
+      
+      
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -24,14 +35,12 @@
     handleLocationError(false, infoWindow, map.getCenter());
   }
       
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: 
-          {lat: 58.152, lng: 8.009},
+       map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 58.152, lng: 8.009},
           zoom: 15
         });
         infoWindow = new google.maps.InfoWindow();
 
- 
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
@@ -39,16 +48,8 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
-  }
-        
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: {lat: 58.152, lng: 8.009},
-          radius: 500,
-          keyword: ['matbutikk']
-        }, callback);
-      
+  }   
+       
 }
       function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -58,14 +59,14 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         }
       }
 
-      function createMarker(place) {
+     function createMarker(place) {
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
           map: map,
           position: place.geometry.location
         });
 
-        google.maps.event.addListener(marker, 'click', function() {
+       google.maps.event.addListener(marker, 'click', function() {
           infowindow.setContent(place.name);
           infowindow.open(map, this);
         });
