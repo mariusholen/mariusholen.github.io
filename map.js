466f1a00c0e42f474509b9f3f01f37be4c70c9d5
mariusholen <35805806+mariusholen@users.noreply.github.com>
2018-05-06 18:57:32 +0200

    Update map.js

--- map.js
+++ map.js
@@ -3,6 +3,12 @@ var map;
       var infowindow;
 
      function initMap() {
+           
+       map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 58.152, lng: 8.009},
+          zoom: 15
+        });
+        infoWindow = new google.maps.InfoWindow();
       
       // Try HTML5 geolocation.
   if (navigator.geolocation) {
@@ -14,7 +20,7 @@ var map;
       
 
      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('Location found');
       infoWindow.open(map);
       map.setCenter(pos);
       
@@ -34,23 +40,16 @@ var map;
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-      
-       map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 58.152, lng: 8.009},
-          zoom: 15
-        });
-        infoWindow = new google.maps.InfoWindow();
-
+}
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+       'Error: The Geolocation service failed.' :
+       'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
   }   
        
-}
       function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
