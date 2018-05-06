d005c22401152baf7518fa6ac1e3bf3183c44370
mariusholen <35805806+mariusholen@users.noreply.github.com>
2018-05-06 18:45:06 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,71 +1,72 @@
- var map;
- var infoWindow;
- var infowindow;
+	var map;
+      var infoWindow;
+      var infowindow;
 
-    function initMap() {
-     
-     // Try HTML5 geolocation.
- if (navigator.geolocation) {
-   navigator.geolocation.getCurrentPosition(function(position) {
+      function initMap() { 
+      
+       // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
-       lat: position.coords.latitude,
-       lng: position.coords.longitude
-     };
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-    infoWindow.setPosition(pos);
-     infoWindow.setContent('Location found');
-     infoWindow.open(map);
-     map.setCenter(pos);
-     
-     infowindow = new google.maps.InfoWindow();
-       var service = new google.maps.places.PlacesService(map);
-       service.nearbySearch({
-         location: (pos),
-         radius: 500,
-         keyword: ['matbutikk']
-       }, callback);
-     
-     
-   }, function() {
-     handleLocationError(true, infoWindow, map.getCenter());
-   });
- } else {
-   // Browser doesn't support Geolocation
-   handleLocationError(false, infoWindow, map.getCenter());
- }
-     
-      map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: 58.152, lng: 8.009},
-         zoom: 15
-       });
-       infoWindow = new google.maps.InfoWindow();
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+      
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: 
+          {lat: 58.152, lng: 8.009},
+          zoom: 15
+        });
+        infoWindow = new google.maps.InfoWindow();
 
+ 
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
- infoWindow.setPosition(pos);
- infoWindow.setContent(browserHasGeolocation ?
-                       'Error: The Geolocation service failed.' :
-                       'Error: Your browser doesn\'t support geolocation.');
- infoWindow.open(map);
- }   
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+  }
+        
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: {lat: 58.152, lng: 8.009},
+          radius: 500,
+          keyword: ['matbutikk']
+        }, callback);
+      
 }
-     function callback(results, status) {
-       if (status === google.maps.places.PlacesServiceStatus.OK) {
-         for (var i = 0; i < results.length; i++) {
-           createMarker(results[i]);
-         }
-       }
-     }
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
 
-    function createMarker(place) {
-       var placeLoc = place.geometry.location;
-       var marker = new google.maps.Marker({
-         map: map,
-         position: place.geometry.location
-       });
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
 
-      google.maps.event.addListener(marker, 'click', function() {
-         infowindow.setContent(place.name);
-         infowindow.open(map, this);
-       });
-     }
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
