e3e40c74bf9352ee4a2e4fa457405b156549b305
mariusholen <35805806+mariusholen@users.noreply.github.com>
2018-05-06 18:19:59 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,31 +1,29 @@
-      var map;
-      var infoWindow;
-      var infowindow;
+var map;
+var infoWindow;
+var infowindow;
 
-     function initMap() {
+function initMap() { 
       
-      // Try HTML5 geolocation.
+       // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-       var pos = {
+      var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      
 
-     infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found');
       infoWindow.open(map);
       map.setCenter(pos);
       
-      infowindow = new google.maps.InfoWindow();
+       infowindow = new google.maps.InfoWindow();
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch({
-          location: pos,
+          location: (pos),
           radius: 500,
           keyword: ['matbutikk']
         }, callback);
-      
       
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
@@ -35,12 +33,12 @@
     handleLocationError(false, infoWindow, map.getCenter());
   }
       
-       map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 58.152, lng: 8.009},
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: 
+          {lat: 58.152, lng: 8.009},
           zoom: 15
         });
         infoWindow = new google.maps.InfoWindow();
-
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
@@ -48,8 +46,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
-  }   
-       
+  }     
 }
       function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -59,14 +56,14 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         }
       }
 
-     function createMarker(place) {
+      function createMarker(place) {
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
           map: map,
           position: place.geometry.location
         });
 
-       google.maps.event.addListener(marker, 'click', function() {
+        google.maps.event.addListener(marker, 'click', function() {
           infowindow.setContent(place.name);
           infowindow.open(map, this);
         });
